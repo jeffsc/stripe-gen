56ceb6d035d25b6b4643e15c7a2a827b313fd704
Jeff Scherpelz <jeffsc@gmail.com>
2013-12-24 10:05:27 -0800

    Added annotations for stripes
    Titles for Header, Body, Footer
    Number of rows for each stripe
    Properly carry remainder to get correct total of stripes
    Little bit of styling

--- js/controllers.js
+++ js/controllers.js
@@ -2,15 +2,15 @@ var stripeGenApp = angular.module('stripeGenApp', []);
 
 stripeGenApp.filter('stripesGen', function() {
   return function(stripeParams) {
-    if (!stripeParams) {
+    if (!angular.isDefined(stripeParams)) {
       return stripeParams;
     }
 
-  console.log('stripesGen', stripeParams);
+  //console.log('stripesGen', stripeParams);
     var s = stripeParams.stripes = stripeParams.stripes || [];
 
-    if (stripeParams.header) {
-      if (!s[0].header) {
+    if (angular.isDefined(stripeParams.header)) {
+      if (s.length < 1 || !s[0].header) {
         s.unshift({ header: true });
       }
       s[0].color = stripeParams.header.color;
@@ -21,7 +21,7 @@ stripeGenApp.filter('stripesGen', function() {
     }
 
     if (stripeParams.footer) {
-      if (!s[s.length - 1].footer) {
+      if (s.length < 1 || !s[s.length - 1].footer) {
         s.push({ footer: true });
       }
       s[s.length - 1].color = stripeParams.footer.color;
@@ -33,12 +33,17 @@ stripeGenApp.filter('stripesGen', function() {
 
     var body = s.slice(stripeParams.header ? 1 : 0,
         stripeParams.footer ? -1 : undefined);
-    var count = 2;
-    if (angular.isDefined(stripeParams.count)) {
-      count = stripeParams.count;
-    }
-    else if (angular.isDefined(stripeParams.word)) {
-      count = stripeParams.word.length;
+    var count = 0;
+    var type = !isNaN(parseInt(stripeParams.count)) ? 'count' :
+      angular.isDefined(stripeParams.word) && stripeParams.word != '' ? 'word' :
+      'none';
+    switch (type) {
+      case 'count':
+        count = stripeParams.count;
+        break;
+      case 'word':
+        count = stripeParams.word.length;
+        break;
     }
 
     if (count < body.length) {
@@ -54,20 +59,31 @@ stripeGenApp.filter('stripesGen', function() {
 
     var colors = _.isEmpty(this.colors) ? [{ color: '000000' }] : this.colors;
     _.each(body, function(s, i) {
+      s.firstBody = false;
       s.color = colors[i % colors.length].color;
-      if (angular.isDefined(stripeParams.count)) {
-        s.height = 1;
-      }
-      else if (angular.isDefined(stripeParams.word)) {
-        s.height = angular.lowercase(stripeParams.word).charCodeAt(i) -
-          'a'.charCodeAt(0);
+      switch (type) {
+        case 'count':
+          s.height = 1;
+          break;
+        case 'word':
+          s.height = angular.lowercase(stripeParams.word).charCodeAt(i) -
+            'a'.charCodeAt(0) + 1;
+          break;
       }
     });
+    if (body.length > 0) {
+      body[0].firstBody = true;
+    }
 
     // Scale stripe heights by total height
     var scale = stripeParams.height / _.reduce(body,
         function(memo, s) { return memo + s.height; }, 0);
-    _.each(body, function(s) { s.height = s.height * scale; });
+    var remainder = 0;
+    _.each(body, function(s) {
+      var h = s.height * scale + remainder;
+      s.height = Math.round(h);
+      remainder = h - s.height;
+    });
 
     return stripeParams;
   }
