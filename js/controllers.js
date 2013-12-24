82e7a602d99a9add94c3bf7889ea87a6617c2aa5
Jeff Scherpelz <jeffsc@gmail.com>
2013-12-24 09:04:46 -0800

    Added stripes based on word
    Distribution of stripes based on letters in word
    Scales to total height
    Fixed bug with $scope.colors not defined
    Added underscore.js
    Ignore .swp files

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,7 @@ stripeGenApp.filter('stripesGen', function() {
       return stripeParams;
     }
 
+  console.log('stripesGen', stripeParams);
     var s = stripeParams.stripes = stripeParams.stripes || [];
 
     if (stripeParams.header) {
@@ -14,7 +15,8 @@ stripeGenApp.filter('stripesGen', function() {
       }
       s[0].color = stripeParams.header.color;
       s[0].height = stripeParams.header.height;
-    } else if (s.length > 0 && s[0].header) {
+    }
+    else if (s.length > 0 && s[0].header) {
       s.shift();
     }
 
@@ -24,13 +26,21 @@ stripeGenApp.filter('stripesGen', function() {
       }
       s[s.length - 1].color = stripeParams.footer.color;
       s[s.length - 1].height = stripeParams.footer.height;
-    } else if (s.length > 0 && s[s.length - 1].footer) {
+    }
+    else if (s.length > 0 && s[s.length - 1].footer) {
       s.pop();
     }
 
     var body = s.slice(stripeParams.header ? 1 : 0,
         stripeParams.footer ? -1 : undefined);
-    var count = stripeParams.count || 2;
+    var count = 2;
+    if (angular.isDefined(stripeParams.count)) {
+      count = stripeParams.count;
+    }
+    else if (angular.isDefined(stripeParams.word)) {
+      count = stripeParams.word.length;
+    }
+
     if (count < body.length) {
       s.splice((stripeParams.header ? 1 : 0) + count, body.length - count);
       body = s.slice(stripeParams.header ? 1 : 0,
@@ -42,12 +52,22 @@ stripeGenApp.filter('stripesGen', function() {
           stripeParams.footer ? -1 : undefined);
     }
 
-    for (var i = 0; i < count; i++) {
-      body[i] = body[i] || {};
-      body[i].color = (this.colors[i % this.colors.length] ||
-          { color: '000000' }).color;
-      body[i].height = stripeParams.height / count;
-    }
+    var colors = _.isEmpty(this.colors) ? [{ color: '000000' }] : this.colors;
+    _.each(body, function(s, i) {
+      s.color = colors[i % colors.length].color;
+      if (angular.isDefined(stripeParams.count)) {
+        s.height = 1;
+      }
+      else if (angular.isDefined(stripeParams.word)) {
+        s.height = angular.lowercase(stripeParams.word).charCodeAt(i) -
+          'a'.charCodeAt(0);
+      }
+    });
+
+    // Scale stripe heights by total height
+    var scale = stripeParams.height / _.reduce(body,
+        function(memo, s) { return memo + s.height; }, 0);
+    _.each(body, function(s) { s.height = s.height * scale; });
 
     return stripeParams;
   }
